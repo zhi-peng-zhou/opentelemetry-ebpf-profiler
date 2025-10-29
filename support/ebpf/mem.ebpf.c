06b20f2e256f15af7550eda449de22ee1826e0c6
zhi-peng-zhou <hey_jepen@163.com>
2025-10-29 06:29:24 +0000

    fix error

--- support/ebpf/mem.ebpf.c
+++ support/ebpf/mem.ebpf.c
@@ -717,13 +717,13 @@ int Py_Free_enter(struct pt_regs *ctx) {
 // func mallocgc(size uintptr, typ *_type, needzero bool) unsafe.Pointer {
 SEC("uprobe/mallocgc_register")
 int mallocgc_register_enter(struct pt_regs *ctx) {
-    u64 *size = (u64 *)PT_REGS_PARM1(ctx);
+    u64 size = (u64)PT_REGS_PARM1(ctx);
 
     u64 id  = bpf_get_current_pid_tgid();
     u32 tid = id & 0xFFFFFFFF;
     u32 pid = id >> 32;
     u64 ts = bpf_ktime_get_ns();
-    return collect_trace(ctx, TRACE_HEAP_ALLOC, pid, tid, ts, 1, *size, 0);
+    return collect_trace(ctx, TRACE_HEAP_ALLOC, pid, tid, ts, 1, size, 0);
 }
 
 SEC("uprobe/mallocgc_stack")
